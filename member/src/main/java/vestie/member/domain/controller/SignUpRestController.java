e7eb08b2bb9c5cf71a105ac0cca4919bb1e952c1
KwakMirae <alfo9490@naver.com>
2022-10-19 00:48:47 +0900

    [FEAT] [#2] 회원가입 시 uri 반환하도록 response 변경

--- member/src/main/java/vestie/member/domain/controller/SignUpRestController.java
+++ member/src/main/java/vestie/member/domain/controller/SignUpRestController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import vestie.member.domain.controller.request.SignUpRequest;
 import vestie.member.domain.service.MemberService;
 
+import java.net.URI;
+
+import static org.springframework.web.util.UriComponentsBuilder.fromPath;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -18,9 +22,13 @@ public class SignUpRestController {
     private final MemberService memberService;
 
     @PostMapping("/v1/signUp")
-    public ResponseEntity.BodyBuilder signUp(@RequestBody SignUpRequest signUpRequest){
-        memberService.signUp(signUpRequest);
-        return ResponseEntity.status(HttpStatus.CREATED);
+    public ResponseEntity<Void> signUp(@RequestBody SignUpRequest signUpRequest){
+        Long memberId = memberService.signUp(signUpRequest);
+
+        URI uri = fromPath("/v1/member/{memberId}") // api/member/{memberId}
+                .buildAndExpand(memberId)      // v1member/10
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
