b08f34af882ad2a82d1fc020f3e6035816c72e2c
shinD <huipulci1@naver.com>
2022-11-06 21:06:13 +0900

    feat: cors 설정

--- member/src/main/java/vestie/member/global/config/CorsConfig.java
+++ member/src/main/java/vestie/member/global/config/CorsConfig.java
@@ -1,20 +1,15 @@
 package vestie.member.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-//    @Bean
-//    public CorsFilter corsFilter(){
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOriginPattern("*");
-//        config.addAllowedHeader("*");
-//        config.addExposedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/api/**", config);
-//        return new CorsFilter(source);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
 }
